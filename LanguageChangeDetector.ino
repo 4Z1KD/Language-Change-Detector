b94baeacd20489c30a177a3d54e209200e360558
Gil (4Z1KD) <4z1kd@iarc.org>
2020-11-19 20:37:55 +0200

    Change language using language buttons

--- LanguageChangeDetector.ino
+++ LanguageChangeDetector.ino
@@ -1,9 +1,27 @@
 char inChar;
 char status;
 
+const int HebrewPIN = 2;    // the number of the hebrew pin
+const int EnglishPIN = 3;    // the number of the english pin
+
+const int HebrewLED = 4;    // the number of the hebrew led
+const int EnglishLED = 13;   // the number of the english
+
 void setup() {
-  pinMode(2, OUTPUT);
-  pinMode(13, OUTPUT);
+  pinMode(HebrewLED, OUTPUT);
+  pinMode(EnglishLED, OUTPUT);
+  pinMode(HebrewPIN, INPUT_PULLUP);
+  pinMode(EnglishPIN, INPUT_PULLUP);
+  
+  //Register to interrupts
+  attachInterrupt(digitalPinToInterrupt(HebrewPIN), SelectHebrew, FALLING);
+  attachInterrupt(digitalPinToInterrupt(EnglishPIN), SelectEnglish, FALLING);
+  
+  //set the interrupt msk
+  PCICR |= (1 << PCIE0);
+  PCMSK0 |= (1 << PCINT2); //Interrupt on pin10
+  PCMSK0 |= (1 << PCINT3); //Interrupt on pin11
+  
   Serial.begin(9600);
 }
 
@@ -20,26 +38,46 @@ void loop() {
       Serial.println("ok");
     }
   }
-
   switch (status)
   {
     case 'e':
-      digitalWrite(2, HIGH);
-      digitalWrite(13, LOW);
+      digitalWrite(HebrewLED, HIGH);
+      digitalWrite(EnglishLED, LOW);
       break;
     case 'j':
-      digitalWrite(2, LOW);
-      digitalWrite(13, HIGH);
+      digitalWrite(HebrewLED, LOW);
+      digitalWrite(EnglishLED, HIGH);
       delay(500);
-      digitalWrite(13, LOW);
+      digitalWrite(EnglishLED, LOW);
       delay(500);
       break;
     case 'h':
-      digitalWrite(2, LOW);
-      digitalWrite(13, HIGH);
+      digitalWrite(HebrewLED, LOW);
+      digitalWrite(EnglishLED, HIGH);
       break;
     default:
       break;
   }
-
+}
+void SelectHebrew()
+{
+  static unsigned long last_ToggleModeInterrupt_time = 0;
+  unsigned long toggleModeInterrupt_time = millis();
+  // If interrupts come faster than 100ms, assume it's a bounce and ignore
+  if (toggleModeInterrupt_time - last_ToggleModeInterrupt_time > 500)
+  {
+    Serial.println("he-IL");
+  }
+  last_ToggleModeInterrupt_time = toggleModeInterrupt_time;
+}
+void SelectEnglish()
+{
+  static unsigned long last_ToggleModeInterrupt_time = 0;
+  unsigned long toggleModeInterrupt_time = millis();
+  // If interrupts come faster than 100ms, assume it's a bounce and ignore
+  if (toggleModeInterrupt_time - last_ToggleModeInterrupt_time > 500)
+  {
+    Serial.println("en-US");
+  }
+  last_ToggleModeInterrupt_time = toggleModeInterrupt_time;
 }
